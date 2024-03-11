ffe1f5d3842fb25b0860192e3a20b67290d2ea49
Ivan <124049176+IvanAGRO@users.noreply.github.com>
2024-03-11 14:25:58 +0100

    Delete 12 directory

--- 12/ej6.sql
+++
@@ -1,7 +0,0 @@
-USE ceutacar;
-
-ALTER TABLE cliente ADD COLUMN emailCliente VARCHAR(50) NOT NULL;
-
-ALTER TABLE cliente DROP COLUMN edad;
-
-ALTER TABLE cliente RENAME COLUMN telefono2 TO telefonoSecundario;
